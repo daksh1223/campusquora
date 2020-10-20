a33dfa30d4b7b4038705b736744ed1d267ce1407
Ayush Kumar Gupta <ayushgupta3401@gmail.com>
2020-10-20 19:13:47 +0530

    removed blogposts

--- campusquora/urls.py
+++ campusquora/urls.py
@@ -19,5 +19,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('authentication.urls')),
-    path('blog/',include('blogposts.urls')),
+    #path('blog/',include('blogposts.urls')),
 ]
